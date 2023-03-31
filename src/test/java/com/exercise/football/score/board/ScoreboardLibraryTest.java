5bdc3ab82d797494ca8c84e4b433d50e7754533a
mrs <maciej.roszyk@gmail.com>
2023-03-31 23:38:20 +0200

    create testclass for library and add junit dependency

---
+++ src/test/java/com/exercise/football/score/board/ScoreboardLibraryTest.java
@@ -0,0 +1,14 @@
+package com.exercise.football.score.board;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class ScoreboardLibraryTest {
+
+    @Test
+    @DisplayName("Start a new game, assuming initial score 0 – 0 and adding it the scoreboard ")
+    public void start_new_match(){
+            
+    }
+
+}
